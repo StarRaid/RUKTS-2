d99f44ce46bdd4c88c9835cbc3b395c9d2fc5e05
StarRaid <trainlover288@gmail.com>
2025-02-09 23:37:28 +0000

    add sheet generator
    added a python script that will generate a sprite sheet for you, with X and Y coords noted, based on: vehicle length, number of liveries, number of articulated parts, number of sprite stack fixtures

---
+++ imagetest.py
@@ -0,0 +1,57 @@
+
+from PIL import Image as img
+import math
+
+base=img.open("templates/lengths.png")
+numbers=[]
+templates=[]
+
+def getdig(a,pos:int) -> int:
+    try:
+        pos+=1
+        a=str(math.floor(a))
+        return int(a[-pos])
+    except:
+        return 0
+
+for i in range (0,10):
+	numbers.append(base.crop((250,i*8,255,(i*8)+7)))
+    
+for i in range (0,8):
+	templates.append(base.crop((0,i*40,250,(i+1)*40)))
+    
+templates.reverse()
+
+outX=int(input("Articulated parts: "))
+outY=int(input("Livery count: "))
+outY+=int(input("Fixture type count: "))
+vehLen=int(input("Vehicle sprite length: "))-1
+
+output = img.new(mode= "RGB", size= (250*outX,40*outY), color=(255,255,255))
+#output.putpalette(base.palette().getdata())
+
+for x in range(0,outX):
+    for y in range(0,outY):
+        
+        #Template
+        coords=(250*x,40*y,250*(x+1),40*(y+1))
+        output.paste(templates[vehLen],coords)
+        
+        #X Digits
+        coords=(250*x+237,40*y+9,250*x+242,40*y+16)
+        output.paste(numbers[getdig(x,1)],coords)
+        coords=(250*x+243,40*y+9,250*x+248,40*y+16)
+        output.paste(numbers[getdig(x,0)],coords)
+        
+        #Y Digits
+        coords=(250*x+237,40*y+28,250*x+242,40*y+35)
+        output.paste(numbers[getdig(y,1)],coords)
+        coords=(250*x+243,40*y+28,250*x+248,40*y+35)
+        output.paste(numbers[getdig(y,0)],coords)
+        
+
+
+# Slut
+
+output.save("output.png")
+
